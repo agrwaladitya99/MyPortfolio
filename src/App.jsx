577b15d23a4cc98431537d14dd42bbaa04f336d4
Aditya Agarwal <agaadi01@CSGICORP.COM>
2024-09-16 01:53:34 +0530

    1. Modified index file. 2. Added global css file. 3. Removed unnecessary files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ContactSection from "./components/ContactSection";
+import HeroSection from "./components/HeroSection";
+import NavBar from "./components/NavBar";
+import PortfolioSection from "./components/PortfolioSection";
+import SkillsSection from "./components/SkillsSection";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <NavBar />
+      <div className="w-[1100px] mx-auto max-[1150px]:w-[1000px] max-[1050px]:w-[900px] max-[950px]:w-[800px] max-[850px]:w-full main-content max-[850px]:flex max-[850px]:flex-col max-[850px]:items-center padding-x-10">
+        <HeroSection />
+        <PortfolioSection />
+        <SkillsSection />
+        <ContactSection />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
